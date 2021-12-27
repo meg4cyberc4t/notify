6af3535d0fc6cde3bb0a8d94cb0fa793860a6358
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2021-12-28 01:18:27 +0300

    The main skeleton of the Search screen
    The version with the main content.
    Third-party patterns or architectures are not used for rendering.
    The automatic focus call on the input field is unpleasant in Indexed Stack, you need to redo it for TabBar and make pages with state preservation

--- lib/screens/mainpage/searchpage.dart
+++ lib/screens/mainpage/searchpage.dart
@@ -1,100 +1,94 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_redux/flutter_redux.dart';
 import 'package:notify/components/snapshot_middleware.dart';
 import 'package:notify/components/widgets/text_field.dart';
 import 'package:notify/components/widgets/user_list_tile.dart';
 import 'package:notify/services/firebase_service.dart';
 import 'package:notify/services/notify_user.dart';
 import 'package:provider/provider.dart';
-import 'package:redux/redux.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
   const SearchPage({Key? key}) : super(key: key);
 
-  List updateState(List state, dynamic action) {
-    if (action is List) {
-      state = action;
-    }
-    return state;
-  }
-
   @override
-  Widget build(BuildContext context) {
-    final store = Store<List>(updateState, initialState: []);
-
-    return StoreProvider<List>(
-      store: store,
-      child: Scaffold(
-        resizeToAvoidBottomInset: false,
-        body: Column(
-          children: [
-            SearchTextField(
-              onChanged: (value) async {
-                var list = await context
-                    .read<FirebaseService>()
-                    .searchFromUsers(value.trim());
-                store.dispatch(list);
-              },
-            ),
-            Expanded(
-              child: StoreConnector<List, List>(
-                  converter: (store) => store.state,
-                  builder: (context, List state) {
-                    if (state is! List<String>) {
-                      return Center(
-                        child: Text(
-                          'Enter something to search for',
-                          style: Theme.of(context).textTheme.headline6,
-                          textAlign: TextAlign.center,
-                        ),
-                      );
-                    }
-                    return StreamBuilder(
-                        stream: context
-                            .read<FirebaseService>()
-                            .getUsersListFromUsersUidList(state),
-                        builder: (context, snapshot) {
-                          var widget = snapshotMiddleware(snapshot);
-                          if (widget != null) {
-                            return widget;
-                          }
-                          List<NotifyUser> data =
-                              snapshot.data as List<NotifyUser>;
-                          return ListView(
-                            children: data
-                                .map((user) => NotifyUserListTile(user: user))
-                                .toList(),
-                          );
-                        });
-                  }),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+  State<SearchPage> createState() => _SearchPageState();
 }
 
-class SearchTextField extends StatelessWidget {
-  const SearchTextField({
-    Key? key,
-    this.onChanged,
-  }) : super(key: key);
-  final ValueChanged<String>? onChanged;
+class _SearchPageState extends State<SearchPage> {
+  TextEditingController controller = TextEditingController();
+  GlobalKey futureBuilderForSearch = GlobalKey();
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(5.0),
-      child: Container(
-          color: Colors.white,
-          child: Padding(
-            padding: const EdgeInsets.all(5.0),
-            child: NotifyTextField(
-              autocorrect: true,
-              hintText: 'Search everyone...',
-              onChanged: onChanged,
-            ),
-          )),
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: Text(
+          'Search',
+          style: Theme.of(context).textTheme.headline3,
+        ),
+        backgroundColor: Theme.of(context).backgroundColor,
+        elevation: 0,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 5, right: 5, bottom: 5),
+            child: Container(
+                color: Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: NotifyTextField(
+                    autofocus: true,
+                    autocorrect: true,
+                    hintText: 'Search everyone...',
+                    controller: controller,
+                    onChanged: (value) => setState(() {}),
+                  ),
+                )),
+          ),
+          Expanded(
+            child: FutureBuilder(
+                key: futureBuilderForSearch,
+                future: context
+                    .read<FirebaseService>()
+                    .searchFromUsers(controller.text.trim()),
+                builder: (context, snapshot) {
+                  var widget = snapshotMiddleware(snapshot);
+                  if (widget != null) {
+                    return widget;
+                  }
+                  var state = snapshot.data as List<String>;
+                  if (state.isEmpty) {
+                    return Center(
+                      child: Text(
+                        'No found',
+                        style: Theme.of(context).textTheme.headline6,
+                        textAlign: TextAlign.center,
+                      ),
+                    );
+                  }
+                  return StreamBuilder(
+                      stream: context
+                          .read<FirebaseService>()
+                          .getUsersListFromUsersUidList(state),
+                      builder: (context, snapshot) {
+                        var widget = snapshotMiddleware(snapshot);
+                        if (widget != null) {
+                          return widget;
+                        }
+                        List<NotifyUser> data =
+                            snapshot.data as List<NotifyUser>;
+                        return ListView(
+                            children: data
+                                .map((user) => NotifyUserListTile(
+                                      user: user,
+                                      key: Key(user.uid),
+                                    ))
+                                .toList());
+                      });
+                }),
+          ),
+        ],
+      ),
     );
   }
 }
