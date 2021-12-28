45cf231666e97385078fd32d1f6f00b9c55272e7
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2021-12-28 23:26:38 +0300

    Some redesign colorPickerPage with new button

--- lib/screens/colorpickerpage.dart
+++ lib/screens/colorpickerpage.dart
@@ -6,7 +6,7 @@ import 'package:flutter_material_color_picker/flutter_material_color_picker.dart
 import 'package:notify/components/widgets/avatar.dart';
 import 'package:notify/components/widgets/notify_direct_button.dart';
 
-Future<Color> pushColorPickerPage(
+Future<Color?> pushColorPickerPage(
     BuildContext context, String title, Color initialValue) async {
   Color? color = await Navigator.push<Color?>(
     context,
@@ -48,77 +48,46 @@ class _ColorPickerPageState extends State<ColorPickerPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Padding(
-      padding: const EdgeInsets.fromLTRB(20, 0, 20, 10),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              "Choose color",
-              style: Theme.of(context).textTheme.headline3,
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 20),
-            Avatar(
-              size: AvatarSize.max,
-              color: selectedColor,
-              title: widget.title,
-            ),
-            const SizedBox(height: 10),
-            MaterialColorPicker(
-              onColorChange: (Color color) {
-                setState(() => selectedColor = color);
-              },
-              selectedColor: selectedColor,
-              colors: Colors.primaries,
-            ),
-            const SizedBox(height: 10),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 8.0, vertical: 10),
-              child: Row(
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  Expanded(
-                    child: NotifyDirectButton(
-                      title: 'Continue',
-                      onPressed: () => Navigator.pop(context, selectedColor),
-                    ),
-                  ),
-                  // Expanded(
-                  //     child: NotifyDirectButton.text(
-                  //   text: 'Continue',
-                  //   onPressed: () => Navigator.pop(context, selectedColor),
-                  // )),
-                ],
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(),
+          Text(
+            "Choose color",
+            style: Theme.of(context).textTheme.headline3,
+            textAlign: TextAlign.center,
+          ),
+          Avatar(
+            size: AvatarSize.max,
+            color: selectedColor,
+            title: widget.title,
+          ),
+          MaterialColorPicker(
+            onColorChange: (Color color) {
+              setState(() => selectedColor = color);
+            },
+            selectedColor: selectedColor,
+            colors: Colors.primaries,
+          ),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              NotifyDirectButton(
+                title: 'Continue',
+                onPressed: () => Navigator.pop(context, selectedColor),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: Row(
-                mainAxisSize: MainAxisSize.max,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Expanded(
-                    child: NotifyDirectButton(
-                      title: 'Continue',
-                      onPressed: () => Navigator.pop(context, selectedColor),
-                    ),
-                  ),
-                  Expanded(
-                      child: NotifyDirectButton(
-                    title: 'Back',
-                    style: NotifyDirectButtonStyle.outlined,
-                    onPressed: () => Navigator.pop(context),
-                  )),
-                ],
+              const SizedBox(height: 10),
+              NotifyDirectButton(
+                title: 'Back',
+                style: NotifyDirectButtonStyle.outlined,
+                onPressed: () => Navigator.pop(context),
               ),
-            ),
-          ],
-        ),
+            ],
+          )
+        ],
       ),
     ));
   }
