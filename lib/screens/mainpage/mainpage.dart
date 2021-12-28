6aa39a29d48f418ad4a4b57dada20ed71cac0471
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2021-12-28 16:18:47 +0300

    Use BottomBar and PageView instead of IndexedPages

--- lib/screens/mainpage/mainpage.dart
+++ lib/screens/mainpage/mainpage.dart
@@ -15,7 +15,10 @@ class MainPage extends StatefulWidget {
   State<MainPage> createState() => _MainPageState();
 }
 
+const tabCount = 4;
+
 class _MainPageState extends State<MainPage> {
+  final _controller = PageController(initialPage: 0);
   int selectedIndex = 0;
 
   @override
@@ -23,61 +26,78 @@ class _MainPageState extends State<MainPage> {
     if (context.watch<User?>() == null) {
       return const AuthPage();
     }
-    final User selectUser = context.watch<User>();
     return Scaffold(
-      body: IndexedStack(
-        index: selectedIndex,
-        children: [
-          const HomePage(),
-          const Scaffold(body: Center(child: Text('likes'))),
-          const SearchPage(),
-          ProfilePage(selectUser.uid),
+      body: PageView(
+        controller: _controller,
+        children: const [
+          HomePage(),
+          Scaffold(body: Center(child: Text('likes'))),
+          SearchPage(),
+          ProfilePage(),
+        ],
+        onPageChanged: (value) => setState(() => selectedIndex = value),
+      ),
+      bottomNavigationBar: NotifyBottomBar(
+        selectedIndex: selectedIndex,
+        onChange: (value) => _controller.jumpToPage(value),
+      ),
+    );
+  }
+}
+
+class NotifyBottomBar extends StatelessWidget {
+  const NotifyBottomBar({
+    Key? key,
+    required this.selectedIndex,
+    required this.onChange,
+  }) : super(key: key);
+  final int selectedIndex;
+  final ValueChanged<int> onChange;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 20,
+            color: Colors.black.withOpacity(.1),
+          )
         ],
       ),
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-              blurRadius: 20,
-              color: Colors.black.withOpacity(.1),
-            )
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+        child: GNav(
+          rippleColor: Colors.grey[300]!,
+          hoverColor: Colors.grey[100]!,
+          gap: 8,
+          tabBorderRadius: 15.0,
+          activeColor: Theme.of(context).primaryColor,
+          iconSize: 24,
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+          duration: const Duration(milliseconds: 400),
+          tabBackgroundColor: Colors.grey[100]!,
+          color: Colors.black,
+          tabs: const [
+            GButton(
+              icon: CupertinoIcons.home,
+              text: 'Home',
+            ),
+            GButton(
+              icon: CupertinoIcons.calendar,
+              text: 'Calendar',
+            ),
+            GButton(
+              icon: CupertinoIcons.search,
+              text: 'Search',
+            ),
+            GButton(
+              icon: CupertinoIcons.person,
+              text: 'Profile',
+            ),
           ],
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
-          child: GNav(
-            rippleColor: Colors.grey[300]!,
-            hoverColor: Colors.grey[100]!,
-            gap: 8,
-            tabBorderRadius: 15.0,
-            activeColor: Theme.of(context).primaryColor,
-            iconSize: 24,
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-            duration: const Duration(milliseconds: 400),
-            tabBackgroundColor: Colors.grey[100]!,
-            color: Colors.black,
-            tabs: const [
-              GButton(
-                icon: CupertinoIcons.home,
-                text: 'Home',
-              ),
-              GButton(
-                icon: CupertinoIcons.calendar,
-                text: 'Calendar',
-              ),
-              GButton(
-                icon: CupertinoIcons.search,
-                text: 'Search',
-              ),
-              GButton(
-                icon: CupertinoIcons.person,
-                text: 'Profile',
-              ),
-            ],
-            selectedIndex: selectedIndex,
-            onTabChange: (value) => setState(() => selectedIndex = value),
-          ),
+          selectedIndex: selectedIndex,
+          onTabChange: onChange,
         ),
       ),
     );
