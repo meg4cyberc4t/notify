6e335ecc60e16f55da4bcb846a59cafead184832
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2022-01-10 22:23:06 +0300

    Update show_edit_field_bottom_sheet.dart

--- lib/components/bottomsheets/show_edit_field_bottom_sheet.dart
+++ lib/components/bottomsheets/show_edit_field_bottom_sheet.dart
@@ -7,24 +7,39 @@ Future<T?> showEditFieldBottomSheet<T>(
   required final String initialValue,
   final String? hintText,
   final String? labelText,
-}) async {
-  final TextEditingController _controller =
-      TextEditingController(text: initialValue);
-  // final double ratio = 100 / MediaQuery.of(context).size.height;
-  await showModalBottomSheet(
-    context: context,
-    builder: (final BuildContext context) => Wrap(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8),
-          child: NotifyTextField(
-            controller: _controller,
-            hintText: hintText,
-            labelText: labelText,
-            autofocus: true,
+}) =>
+    showModalBottomSheet(
+      context: context,
+      isDismissible: true,
+      backgroundColor: Colors.transparent,
+      isScrollControlled: true,
+      builder: (final BuildContext context) => AnimatedPadding(
+        padding: EdgeInsets.only(
+          bottom: MediaQuery.of(context).viewInsets.bottom,
+        ),
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeOut,
+        child: Container(
+          height: 100,
+          color: Colors.transparent,
+          child: Container(
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Theme.of(context).backgroundColor,
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(15),
+                topRight: Radius.circular(15),
+              ),
+            ),
+            child: NotifyTextField(
+              hintText: hintText,
+              labelText: labelText,
+              autofocus: true,
+              initialValue: initialValue,
+              onFieldSubmitted: (final String? value) =>
+                  Navigator.of(context).pop(value),
+            ),
           ),
         ),
-      ],
-    ),
-  );
-}
+      ),
+    );
