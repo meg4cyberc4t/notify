8e37f815d3718bed32c267a514bbc2ed9a38d52d
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2022-02-03 18:08:29 +0300

    Add auth_preview and sign_up_view in onGenerateRoute

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:notify/src/pages/auth/auth_preview.dart';
+import 'package:notify/src/pages/auth/sign_up_view.dart';
 import 'package:notify/src/pages/brand_book_page.dart';
+import 'package:notify/src/pages/color_picker_view.dart';
 import 'package:notify/src/settings/theme_data_service.dart';
 
 import 'settings/settings_controller.dart';
@@ -27,10 +30,33 @@ class MyApp extends StatelessWidget {
           darkTheme: NotifyThemeData.darkThemeData,
           theme: NotifyThemeData.lightThemeData,
           themeMode: settingsController.themeMode,
+          initialRoute: AuthPreview.routeName,
           onGenerateRoute: (RouteSettings routeSettings) {
-            return MaterialPageRoute<void>(
+            Map<String, dynamic>? args =
+                routeSettings.arguments as Map<String, dynamic>?;
+            if (routeSettings.name == ColorPickerView.routeName) {
+              return MaterialPageRoute<Color>(
+                settings: routeSettings,
+                builder: (BuildContext context) => ColorPickerView(
+                  title: args!['title'],
+                  initialValue: args['color'],
+                ),
+              );
+            }
+            return MaterialPageRoute(
               settings: routeSettings,
               builder: (BuildContext context) {
+                switch (routeSettings.name) {
+                  case AuthPreview.routeName:
+                    return const AuthPreview();
+                  case SignUpView.routeName:
+                    return const SignUpView();
+                  case ColorPickerView.routeName:
+                    return ColorPickerView(
+                      title: args!['title'],
+                      initialValue: args['color'],
+                    );
+                }
                 return const BrandBookPage();
               },
             );
