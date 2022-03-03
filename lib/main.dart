af163cfa3c2e7d790e0306cbf8d7ddd1828bffa1
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2022-03-03 22:20:37 +0300

    Update SettingsService init

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:notify/src/settings/settings_service.dart';
 
 import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
-import 'src/settings/settings_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  final settingsController = SettingsController(SettingsService());
+  await SettingsService.instance.init();
+  final settingsController = SettingsController();
   await settingsController.loadSettings();
   runApp(MyApp(settingsController: settingsController));
 }
