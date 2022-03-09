b6a44195c5e4c7b3701f913d11c1e6a25737b8b4
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2022-03-09 14:08:44 +0300

    Update home_view.dart

--- lib/src/pages/home/home_view.dart
+++ lib/src/pages/home/home_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:notify/src/components/local_future_builder.dart';
+import 'package:notify/src/components/notification_list_tile.dart';
+import 'package:notify/src/components/show_delete_dialog.dart';
 import 'package:notify/src/models/notify_notification_quick.dart';
 import 'package:notify/src/pages/additional/create_edit_notification_view.dart';
 import 'package:notify/src/settings/api_service/api_service.dart';
@@ -12,35 +14,59 @@ class HomeView extends StatefulWidget {
   State<HomeView> createState() => _HomeViewState();
 }
 
-class _HomeViewState extends State<HomeView> {
+class _HomeViewState extends State<HomeView>
+    with AutomaticKeepAliveClientMixin {
+  @override
+  bool get wantKeepAlive => true;
   @override
   Widget build(BuildContext context) {
+    super.build(context);
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Notifications'),
+          title: const Text('Notifications today'),
         ),
-        body: LocalFutureBuilder(
-          future: ApiService.notifications.get(),
-          onData: (BuildContext context, List<NotifyNotificationQuick> list) {
-            debugPrint(list.length.toString());
-            return ListView.builder(
-                itemCount: list.length,
-                itemBuilder: (BuildContext context, int index) {
-                  NotifyNotificationQuick ntf = list[index];
-                  return ListTile(
-                    title: Text(ntf.title),
-                    subtitle: ntf.description.isNotEmpty
-                        ? Text(ntf.description)
-                        : null,
-                    onTap: () {},
-                  );
-                });
-          },
-          onError: (BuildContext context, Object error) {
-            return Text(error.toString());
-          },
-          onProgress: (BuildContext context) => const Center(
-            child: Text('Progress'),
+        body: RefreshIndicator(
+          onRefresh: () async => setState(() {}),
+          child: LocalFutureBuilder(
+            future: ApiService.notifications.get(),
+            onData: (BuildContext context, List<NotifyNotificationQuick> list) {
+              list.sort((a, b) => b.deadline.compareTo(a.deadline));
+              final ntfs = list.where(
+                  (element) => element.deadline.day == DateTime.now().day);
+              if (ntfs.isEmpty) {
+                return const SizedBox(
+                  height: 300,
+                  child: Center(
+                    child: Text('There are no reminders today'),
+                  ),
+                );
+              }
+              return ListView(
+                children: ntfs
+                    .map(
+                      (e) => NotificationListTile(
+                          notification: e,
+                          onTap: () {},
+                          onLongPress: () async {
+                            showDeleteDialog(context: context, title: e.title)
+                                .then((value) async {
+                              if (value != null && value) {
+                                await ApiService.notifications
+                                    .delete(uuid: e.id);
+                                setState(() {});
+                              }
+                            });
+                          }),
+                    )
+                    .toList(),
+              );
+            },
+            onError: (BuildContext context, Object error) {
+              return Text(error.toString());
+            },
+            onProgress: (BuildContext context) => const Center(
+              child: Text('Progress'),
+            ),
           ),
         ),
         floatingActionButton: FloatingActionButton(
