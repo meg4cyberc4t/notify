daf68f52e414fe8b4925408d8a4286b8811c92ab
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2022-03-11 17:18:41 +0300

    Update sign_up_view.dart

--- lib/src/pages/auth/sign_up_view.dart
+++ lib/src/pages/auth/sign_up_view.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_single_quotes
-
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -7,7 +5,6 @@ import 'package:notify/src/methods/get_passive_color.dart';
 import 'package:notify/src/pages/additional/color_picker_view.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:notify/src/pages/home/home_view.dart';
-import 'package:notify/src/pages/router_view.dart';
 import 'package:notify/src/settings/api_service/api_service.dart';
 import 'package:notify/src/settings/api_service/middleware/api_service_exception.dart';
 
