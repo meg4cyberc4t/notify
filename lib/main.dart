ad1a7c97529d51ec782ad0482498acf359e975a1
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2022-03-13 14:28:51 +0300

    Add fonts in assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:notify/src/settings/notifications_service.dart';
 import 'package:notify/src/settings/settings_service.dart';
-
 import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  LicenseRegistry.addLicense(() async* {
+    final String license = await rootBundle.loadString('assets/fonts/OFL.txt');
+    yield LicenseEntryWithLineBreaks(<String>['google_fonts'], license);
+  });
+  GoogleFonts.config.allowRuntimeFetching = false;
   await Firebase.initializeApp();
   await NotificationService.initializingSettings();
   await SettingsService.instance.init();
