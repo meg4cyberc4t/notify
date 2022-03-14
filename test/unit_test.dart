7143c404bde813574b239619f938f9e431e22f85
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2022-03-14 18:06:14 +0300

    Remove empty test

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,10 +1,3 @@
 import 'package:flutter_test/flutter_test.dart';
 
-void main() {
-  group('Plus Operator', () {
-    test('should add two numbers together', () {
-      expect(1 + 1, 2);
-      // The tests will be later
-    });
-  });
-}
+void main() {}
