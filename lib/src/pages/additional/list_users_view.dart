09eeb5c8baa105fe9229b97c5c43c4de3cdc6cc7
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2022-03-19 19:33:33 +0300

    Update list_users_view.dart

--- lib/src/pages/additional/list_users_view.dart
+++ lib/src/pages/additional/list_users_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:notify/src/components/local_future_builder.dart';
 import 'package:notify/src/components/view_models/user_list_tile.dart';
 import 'package:notify/src/models/notify_user_quick.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class ListUsersView extends StatefulWidget {
   const ListUsersView({
@@ -28,24 +29,41 @@ class _ListUsersViewState extends State<ListUsersView>
     return Scaffold(
       appBar: AppBar(title: Text(widget.title)),
       body: LocalFutureBuilder<List<NotifyUserQuick>>(
-        future: widget.callback(),
-        onError: (BuildContext context, Object error) {
-          debugPrint(error.toString());
-          return const Center(
-            child: Text('Error'),
-          );
-        },
-        onProgress: (BuildContext context) =>
-            const Center(child: CircularProgressIndicator()),
-        onData: (BuildContext context, List<NotifyUserQuick> users) =>
-            ListView.builder(
-          itemCount: users.length,
-          itemBuilder: (context, index) => UserListTile(
-            user: users[index],
-            onTap: widget.onSelect,
-          ),
-        ),
-      ),
+          future: widget.callback(),
+          onError: (BuildContext context, Object error) {
+            debugPrint(error.toString());
+            return const Center(
+              child: Text('Error'),
+            );
+          },
+          onProgress: (BuildContext context) =>
+              const Center(child: CircularProgressIndicator()),
+          onData: (BuildContext context, List<NotifyUserQuick> users) {
+            if (users.isEmpty) {
+              return SizedBox(
+                height: 300,
+                child: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Icon(Icons.search_off_outlined),
+                      Text(
+                        AppLocalizations.of(context)!.notFound,
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            }
+            return ListView.builder(
+              itemCount: users.length,
+              itemBuilder: (context, index) => UserListTile(
+                user: users[index],
+                onTap: widget.onSelect,
+              ),
+            );
+          }),
     );
   }
 }
