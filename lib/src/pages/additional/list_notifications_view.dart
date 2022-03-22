1633d19e35b66503f156bb50d7c133d2881e7c20
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2022-03-22 17:54:53 +0300

    Add top paddings in notifications_view

--- lib/src/pages/additional/list_notifications_view.dart
+++ lib/src/pages/additional/list_notifications_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notify/src/components/local_future_builder.dart';
+import 'package:notify/src/components/local_splitter.dart';
 import 'package:notify/src/components/view_models/notification_list_tile.dart';
 import 'package:notify/src/models/notify_notification_quick.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -58,9 +59,16 @@ class _ListNotificationsViewState extends State<ListNotificationsView>
             }
             return ListView.builder(
               itemCount: ntfs.length,
-              itemBuilder: (context, index) => NotificationListTile(
-                notification: ntfs[index],
-                onTap: widget.onSelect,
+              itemBuilder: (context, index) => LocalSplitter(
+                split: index == 0,
+                splitter: (Widget widget) => Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: widget,
+                ),
+                child: NotificationListTile(
+                  notification: ntfs[index],
+                  onTap: widget.onSelect,
+                ),
               ),
             );
           }),
